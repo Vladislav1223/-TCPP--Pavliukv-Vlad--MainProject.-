f773d7a8280a4a7059864418d57187c4ddd925f7
Vladislav <vlad.king.20012109@gmail.com>
2021-03-16 23:11:26 +0200

    Изменения с IDE

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -10,6 +10,8 @@ namespace lab2
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Vlad!");
+            
         }
     }
 }
